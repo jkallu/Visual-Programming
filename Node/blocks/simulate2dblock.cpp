0c80d0985989ef89d0f5d8de280e1707efdcf51b
Jinesh Kallunkathariyil <jinesh.bond@gmail.com>
2019-06-24 21:13:32 +0200

    files moved to sub folders

---
+++ Node/blocks/simulate2dblock.cpp
@@ -0,0 +1,6 @@
+#include "simulate2dblock.h"
+
+Simulate2DBlock::Simulate2DBlock()
+{
+
+}
---
+++ Node/blocks/simulate2dblock.h
@@ -0,0 +1,11 @@
+#ifndef SIMULATE2DBLOCK_H
+#define SIMULATE2DBLOCK_H
+
+
+class Simulate2DBlock
+{
+public:
+    Simulate2DBlock();
+};
+
+#endif // SIMULATE2DBLOCK_H
